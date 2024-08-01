65a35512f90dbcea19c0f4c72aa48e92c16e2a1f
AyushRay06 <rayayush47@gmail.com>
2024-08-02 02:13:22 +0530

    Code-generation page init

---
+++ app/api/code/route.ts
@@ -0,0 +1,49 @@
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+import OpenAI from "openai"
+import { ChatCompletion } from "openai/resources/index.mjs"
+
+//const configuration = new Configuration({
+//    apiKey:process.env.OPENAI_API_KEY,
+//})
+
+//const openai = new OpenAIApi(configuration)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const instructionMessgae: OpenAI.Chat.ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+}
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth()
+    const body = await req.json()
+    const { messages } = body
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API key not configured", { status: 500 })
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 })
+    }
+
+    const response = await openai.chat.completions.create({
+      //messages: { role: "system", content: "You are a helpful assistant." }],
+      messages: [instructionMessgae, ...messages],
+      model: "gpt-3.5-turbo",
+    })
+    //return NextResponse.json(response.data.choices[0].message)
+    return new NextResponse(JSON.stringify(response.choices[0].message))
+  } catch (error) {
+    console.log("CODE_ERROR", error)
+    return new NextResponse("Internalerror", { status: 500 })
+  }
+}
--- components/empty.tsx
+++ components/empty.tsx
@@ -8,7 +8,7 @@ export const Empty = ({ label }: EmptyProps) => {
   return (
     <div className="h-full p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-        <Image alt="Empty" src="/empty.png" fill />
+        <Image alt="Empty" src="/1.png" fill />
       </div>
       <p className="text-muted-foreground text-sm">{label}</p>
     </div>
