70a1cd43aa28cfcede236635fb82f8f18e967312
AyushRay06 <rayayush47@gmail.com>
2024-08-04 00:30:13 +0530

    pro-modal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import { useProModal } from "@/hooks/use-pro-model"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog"
+import { Badge } from "./ui/badge"
 
 export const ProMoadal = () => {
+  const proMoadal = useProModal()
   return (
-    <Dialog open>
+    <Dialog open={proMoadal.isOpen} onOpenChange={proMoadal.onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex items-center justify-center flex-col gap-y-4 pb-2">
-            Upgrade To{" "}
+            Upgrade To Dr Doom
+            <Badge className="uppercase text-sm py-1">Pro</Badge>
+            <div className="flex items-center gap-x-2 font-bold py-1"></div>
           </DialogTitle>
         </DialogHeader>
       </DialogContent>
